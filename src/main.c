f8742648e2b97c0851ac71e73a96a30954fa363e
Viktor Tiulpin <tiulpin@users.noreply.github.com>
2017-04-23 20:39:01 +0300

    calc_v0.1.0
    stage00:
    - ок, идём дальше
    
    stage01:
    - ок, идем дальше (20 пройдено)
    
    stage02:
    - ошибка, неверный ответ в выражении
    - возможная причина - отсутствует вычислительный модуль, проверьте 3.x.x
    - на тесте № 2

--- src/main.c
+++ src/main.c
@@ -1,14 +1,13 @@
 #pragma warning(disable:4996)
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 // TODO: Include local header files
 
 // TODO: Use all unused parameters and remove this temporary definition
 // Helps eliminating warning C4100: 'error': unreferenced formal parameter
-#define UNUSED_PARAMETER(name) (void)name
 
 //////////////////////////////////////////////////////////////////////////////
 // Dummy calc module
@@ -16,95 +15,125 @@
 // TODO: Move to a separate module 'calc'
 typedef enum
 {
-  ERR_OK,
-  ERR_NOT_ENOUGH_MEMORY,
+    ERR_OK,
+    ERR_NOT_ENOUGH_MEMORY,
+    ERR_BRACKET,
+    ERR_WRONG_EXPRESSION,
   // TODO: Add your own error codes
 } error_t;
-
+error_t ReportError(error_t error);
 // TODO: Move to a separate module 'calc'
 char const* GetErrorString(error_t error)
 {
-  // TODO: Find the corresponding error description
-  UNUSED_PARAMETER(error);
-  return "";
+    // TODO: Find the corresponding error description
+    switch (error) {
+        default:
+            return "UNDEFINED";
+    }
 }
 
 // TODO: Move to a separate module 'calc'
-double Calculate(char const* expression, error_t* error)
+double Calculate(char const* expression, error_t* lastError)
 {
-  double result;
-
-  // TODO: Replace with a computational algorithm subdivided into modules/functions
-  UNUSED_PARAMETER(expression);
-  result = 4.0;
-  if (error != NULL)
-    *error = ERR_OK;
-
-  return result;
+    double result;
+    result = 4.0;
+    return result;
 }
 
 //////////////////////////////////////////////////////////////////////////////
 // UI functions
+int NeedCalculate(char const*);
 
-char* ReadLine(FILE* in)
+//TODO: Move code to functions
+char* ReadLine(FILE* in, error_t* lastError)
 {
-  char* line = NULL;
-  // TODO: Read a line of text into a dynamic memory block
-  UNUSED_PARAMETER(in);
-  return line;
+    char* buffer = NULL;
+    char* line = (char*) malloc(sizeof(char));
+    int i = 0;
+    line[i] = (char)fgetc(in);
+    if (line[i] == EOF)
+    {
+        free(line);
+        return NULL;
+    }
+    while ((line[i] != '\n'))
+    {
+        if (line[i] == EOF) break;
+        i++;
+        buffer = (char*) realloc(line, sizeof(char) * (i + 1));
+        if (buffer == NULL)
+        {
+            *lastError = ERR_NOT_ENOUGH_MEMORY;
+            return line;
+        }
+        else line = buffer;
+        line[i] = (char)fgetc(in);
+    }
+    line[i] = '\0';
+    if ((NeedCalculate(line)) && ((line[i-1] == '+' || line[i-1] == '-') || (line[i-1] == '*' || line[i-1] == '/') || (line[i-1] == '(' || line[i-1] == ')')) && (line[i - 1]!=')'))
+            *lastError = ERR_WRONG_EXPRESSION;
+    return line;
 }
 
-int NeedCalculate(char const* line)
-{
-  // TODO: Determine if the line contains an expression
-  UNUSED_PARAMETER(line);
-  return 1;
+
+int NeedCalculate(char const *line) {
+    int i = 0;
+    for(i; ((line[i] != '\0') && !(((line[i] == '+' || line[i] == '-') || (line[i] == '*' || line[i] == '/') || (line[i] == '(' || line[i] == ')'))) && !(isalnum(line[i]))); i++);
+    if (line[i] == '\0') return 0;
+    if (line[i] == '/' && line[i + 1] == '/') return 0;
+    for (i; line[i] != '\0'; i++) if (((line[i] == '+' || line[i] == '-') || (line[i] == '*' || line[i] == '/') || (line[i] == '(' || line[i] == ')'))||isalnum(line[i])) return 1;
+    return 0;
 }
 
 error_t ReportError(error_t error)
 {
-  printf("ERROR: %s\n", GetErrorString(error));
-  return error;
+    printf("ERROR: %s\n", GetErrorString(error));
+    return error;
 }
 
-void ProcessLine(char const* line)
+void ProcessLine(char const* line, error_t *lastError)
 {
-  error_t lastError = ERR_OK;
-  if (!NeedCalculate(line))
-  {
-    puts(line);
-    return;
-  }
-
-  printf("%s == ", line);
-  double result = Calculate(line, &lastError);
-  if (lastError == ERR_OK)
-    printf("%lg\n", result);
-  else
-    ReportError(lastError);
+    if (!NeedCalculate(line))
+    {
+        puts(line);
+        return;
+    }
+    printf("%s == ", line);
+    double result = Calculate(line, lastError);
+    if (*lastError == ERR_OK) printf("%lg\n", result);
+    else{
+        ReportError(*lastError);
+        *lastError = ERR_OK;
+    }
+    *lastError = ERR_OK;
 }
 
 int main(int argc, char const* argv[])
 {
-  FILE* in = stdin;
-  char* line = NULL;
+    FILE* in = stdin;
+    char* line = NULL;
+    error_t lastError = ERR_OK;
+    // Choose an input source
+    if (argc > 1 && (in = fopen(argv[1], "r")) == NULL || argc > 2)
+    {
+        printf("ERROR: Cannot open file '%s'", argv[1]);
+        return -1;
+    }
+    else if (argc == 2) {
+        in = freopen(argv[1], "r", stdin);
+        if (in == NULL) {
+            printf("ERROR: Cannot open file");
+            return -1;
+        }
+    }
+    // Process the data line by line
+    while ((line = ReadLine(in, &lastError)) != NULL)
+    {
+        ProcessLine(line, &lastError);
+        free(line);
+    }
 
-  // Choose an input source
-  if (argc > 1 && (in = fopen(argv[1], "r")) == NULL)
-  {
-    printf("ERROR: Cannot open file '%s'", argv[1]);
-    return -1;
-  }
-
-  // Process the data line by line
-  while ((line = ReadLine(in)) != NULL)
-  {
-    ProcessLine(line);
-    free(line);
-  }
-
-  // Clean up
-  if (in != stdin)
-    fclose(in);
-  return 0;
+    // Clean up
+    if (in != stdin) fclose(in);
+    return 0;
 }
