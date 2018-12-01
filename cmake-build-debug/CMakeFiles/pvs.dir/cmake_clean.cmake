08677d8446b9d96e8b8cdbefb5af43b8ed4c41aa
Viktor Tiulpin <tq@pm.me>
2018-12-01 12:42:00 +0300

    initial commit

---
+++ cmake-build-debug/CMakeFiles/pvs.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pvs"
+  "calc-tiulpin.plog"
+  "PVS-Studio/sources/ProcessLine.c.plog"
+  "PVS-Studio/sources/Convert.c.plog"
+  "PVS-Studio/sources/Calculate.c.plog"
+  "PVS-Studio/sources/Stack.c.plog"
+  "PVS-Studio/sources/Main.c.plog"
+  "PVS-Studio.cfg"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/pvs.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
